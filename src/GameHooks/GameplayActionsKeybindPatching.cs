2cf6fba4a934f44ebfe4286cb6fb8ab2e10ea219
Torphedo <ahiatt.cosgrove@gmail.com>
2022-06-08 19:40:47 -0400

    Setup for GameplayActions patching
    I can't get it to compile the commented out line, which is pulled directly from the 1.0 DLL. Not sure why.

---
+++ src/GameHooks/GameplayActionsKeybindPatching.cs
@@ -0,0 +1,15 @@
+﻿
+using BBI.Unity.Game;
+using HarmonyLib;
+using System;
+using InControl;
+using UnityEngine;
+
+namespace Torphedo.Shipbreaker.Cheats
+{
+    [HarmonyPatch(typeof(GameplayActions), "GameplayActions", MethodType.Getter)]
+    public static class GameplayActionsKeybindPatches
+    {
+        // GameplayActions.mDefaultBindings.Add(new LynxControlBindingInfo(GameplayActions.GameplayActionSet.ToggleFramerate, InputControlType.None, new Key[] { Key.F4 }, Mouse.None));
+    }
+}
