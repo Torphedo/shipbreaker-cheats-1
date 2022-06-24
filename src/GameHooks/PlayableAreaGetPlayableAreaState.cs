5d91d84d0b730e6a145cb1de5a423575cd8a5d76
Torphedo <ahiatt.cosgrove@gmail.com>
2022-06-24 01:30:23 -0400

    Add out of bounds cheat

---
+++ src/GameHooks/PlayableAreaGetPlayableAreaState.cs
@@ -0,0 +1,16 @@
+﻿
+using BBI.Unity.Game;
+using HarmonyLib;
+
+namespace Torphedo.Shipbreaker.Cheats
+{
+    [HarmonyPatch(typeof(PlayableArea), "GetPlayableAreaState")]
+    public static class PlayableAreaGetPlayableAreaState
+    {
+        public static bool Prefix(ref PlayableArea.PlayableAreaState __result)
+        {
+            __result = PlayableArea.PlayableAreaState.Safe;
+            return false;
+        }
+    }
+}
